18724e05c827dfb040b7029de78890705ef64431
easyfun2 <caixiaowei0001@126.com>
2020-11-19 11:18:31 +0800

    Initial Commit

---
+++ miniprogram/pages/home/home.js
@@ -0,0 +1,72 @@
+// pages/home/home.js
+
+// Page()由小程序原生提供，它是一个函数，
+// 用于初始化一个页面实例。它的参数是一个配置对象，
+// 用于设置当前页面的行为属性。这里是一个空对象，表示不设置任何属性
+
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
